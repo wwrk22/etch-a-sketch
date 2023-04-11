b26bb3eabb5af8616c2f23dbd0593a190c0b9eee
Won Rhim <wwrk22@gmail.com>
2023-04-10 22:52:15 -0400

    Add mouseover style toggle that colors a square

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const SQUARE_WIDTH = 44;
+
 // Create a grid of 16x16 squares.
 const grid = document.querySelector('#grid');
 
@@ -7,11 +9,31 @@ for (let i = 0; i < 256; i++) {
   grid.appendChild(square);
 }
 
+// Add initial style and event listeners to all squares.
 const squares = document.querySelectorAll('.square');
-squares.forEach(square => addSquareStyle(square));
+squares.forEach(square => setupSquare(square));
 
-function addSquareStyle(square) {
-  square.style['background-color'] = '#DEECD8';
-  square.style['width'] = '40px';
-  square.style['height'] = '40px';
+function setupSquare(square) {
+  addMouseEventListeners(square);
+}
+
+function addMouseEventListeners(square) {
+  square.addEventListener('mouseover', () => toggleMouseoverStyle(square));
+}
+
+function toggleMouseoverStyle(square) {
+  square.classList.toggle('mouseover-square');
+}
+
+// Prompt user for new grid size, then create the grid.
+const btn = document.querySelector('#new-grid-btn');
+
+btn.addEventListener('click', createNewGrid);
+
+function createNewGrid() {
+  let numSquares = prompt("Enter number of squares per side");
+  numSquares = (numSquares > 100) ? 100 : numSquares;
+
+  const gridWidth = SQUARE_WIDTH * numSquares;
+  drawGrid(gridWidth);
 }
