afecdfeda6c54147b5352ee8fd9d92ab025c387b
Won Rhim <wwrk22@gmail.com>
2023-04-11 22:24:03 -0400

    Refactor

--- index.js
+++ index.js
@@ -39,7 +39,6 @@ function createNewGrid() {
 
 // Square functions
 function setupSquare(square) {
-  square.dataset.darken = '0';
   square.classList.toggle('square'); // Initial style
   addMouseoverListener(square);
 }
@@ -49,26 +48,32 @@ function addMouseoverListener(square) {
 }
 
 function colorSquare(square) {
-  const darkenValue = +square.dataset.darken;
-
   if (square.style.backgroundColor === "") {
-    const red = Math.floor(Math.random() * 256);
-    const green = Math.floor(Math.random() * 256);
-    const blue = Math.floor(Math.random() * 256);
-    square.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`; 
-    square.setAttribute(INC_RED_HUE, (red / 10));
-    square.setAttribute(INC_GREEN_HUE, (green / 10));
-    square.setAttribute(INC_BLUE_HUE, (blue / 10));
-  } else {
-    const r = /\d+/g;
-    let red = +(r.exec(square.style.backgroundColor)[0]) - +square.getAttribute(INC_RED_HUE);
-    let green = +(r.exec(square.style.backgroundColor)[0]) - +square.getAttribute(INC_GREEN_HUE);
-    let blue = +(r.exec(square.style.backgroundColor)[0]) - +square.getAttribute(INC_BLUE_HUE);
-    red = (red < 0) ? 0 : red;
-    green = (green < 0) ? 0 : green;
-    blue = (blue < 0) ? 0 : blue;
-    square.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    initialColor(square);
+  } else if (square.style.backgroundColor !== "rgb(0, 0, 0)") {
+    incrementalColor(square);
   }
+}
+
+function initialColor(square) {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  square.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`; 
+  square.setAttribute(INC_RED_HUE, (red / 10));
+  square.setAttribute(INC_GREEN_HUE, (green / 10));
+  square.setAttribute(INC_BLUE_HUE, (blue / 10));
+}
+
+function incrementalColor(square) {
+  const r = /\d+/g;
+  let red = +(r.exec(square.style.backgroundColor)[0]) - +square.getAttribute(INC_RED_HUE);
+  let green = +(r.exec(square.style.backgroundColor)[0]) - +square.getAttribute(INC_GREEN_HUE);
+  let blue = +(r.exec(square.style.backgroundColor)[0]) - +square.getAttribute(INC_BLUE_HUE);
+  red = (red < 0) ? 0 : red;
+  green = (green < 0) ? 0 : green;
+  blue = (blue < 0) ? 0 : blue;
+  square.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 }
 
 // Prompt user for new grid size, then create the grid.
